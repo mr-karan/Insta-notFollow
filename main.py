0efb12e3f34f4b7055e470e5ae863b186040b037
Karan Sharma <karansharma1295@gmail.com>
2015-11-24 04:26:20 +0530

    authentication added

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import requests
-from flask import Flask
-from flask import request, render_template
-
+from instagram.client import InstagramAPI
+from flask import Flask, request, render_template, session, redirect, abort, flash, jsonify
+import os
 app = Flask(__name__)
+app.secret_key = os.environ.get('FLASK_SECRET_KEY')
 
-from instafollow import (get_user_id,get_unique)
-
+# configure Instagram API
+instaConfig = {
+    'client_id':os.environ.get('INSTA_CLIENT_ID'),
+    'client_secret':os.environ.get('INSTA_CLIENT_SECRET'),
+    'redirect_uri' : 'http://insta-notfollow.herokuapp.com/instagram_callback'
+}
+api = InstagramAPI(**instaConfig)
+'''
 @app.route('/')
 def index():
     error_message = 'Username not found'
@@ -18,6 +25,76 @@ def index():
         return render_template('result.html', username=username,
                                result=result)
     return render_template('index.html', error_message=error_message)
+'''
+@app.route('/')
+def index():
+
+    # if instagram info is in session variables, then display user photos
+    if 'instagram_access_token' in session and 'instagram_user' in session:
+        userAPI = InstagramAPI(access_token=session['instagram_access_token'])
+        follows = []
+        follows, next_ = userAPI.user_follows(user_id=session['instagram_user'].get('id'))
+        while next_:
+            more_follows, next_ = userAPI.user_follows(with_next_url=next_)
+            follows.extend(more_follows)
+        
+        followed_by = []
+        followed_by, _ = userAPI.user_followed_by(user_id=session['instagram_user'].get('id'))
+        while _:
+            more_followed_by, _ = api.user_followed_by(with_next_url=_)
+            followed_by.extend(more_followed_by)
+
+        followers_names=list(map(str,follows))
+        followed_by_names=list(map(str,followed_by))
+        unique_people=list(set(followers_names) - set(followed_by_names))
+        clean_list=[i.replace("User: ","") for i in unique_people]
+        result=[i for i in follows if i.username in clean_list]
+        resultattr = {}
+        for i in result:
+            resultattr[i.username]=i.profile_picture
+
+
+        return render_template('result.html', result = resultattr)
+        
+
+    else:
+
+        return render_template('index.html')
+
+@app.route('/connect')
+def main():
+
+    url = api.get_authorize_url(scope=["follower_list"])
+    return redirect(url)
+
+@app.route('/instagram_callback')
+def instagram_callback():
+
+    code = request.args.get('code')
+
+    if code:
+
+        access_token, user = api.exchange_code_for_access_token(code)
+        if not access_token:
+            return 'Could not get access token'
+
+        app.logger.debug('got an access token')
+        app.logger.debug(access_token)
+
+        # Sessions are used to keep this data 
+        session['instagram_access_token'] = access_token
+        session['instagram_user'] = user
+
+        return redirect('/') # redirect back to main page
+        
+    else:
+        return "Uhoh no code provided"
+
+
+    
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
--- settings.py
+++ settings.py
@@ -3,3 +3,6 @@ import os
 access_token = os.environ['INSTA_ACCESS_TOKEN']
 client_id = os.environ['INSTA_CLIENT_ID']
 base_url='https://api.instagram.com/v1'
+client_secret=os.environ['INSTA_CLIENT_SECRET']
+secret_key=os.environ['FLASK_SECRET_KEY']
+REDIRECT_URI='http://localhost:5000/instagram_callback'
